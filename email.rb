6e0a6e9816cac0050afecc6beb65994350b332e0
JingSen <jsliu521@gmail.com>
2021-06-17 17:29:33 +0800

    update email setting

--- email.rb
+++ email.rb
@@ -19,12 +19,12 @@ def send_email(to, subject, html)
   puts 'begin to send email...'
 	mail = MailFactory.new
 	mail.to = to.join(',')
-	mail.from = 'DailyBuild@thc.net.cn'
+	mail.from = 'jsliu@thc.net.cn'
 	mail.subject = subject
 	mail.html = html
 	
-	Net::SMTP.start('mail.thc.net.cn') { |smtp|
-    smtp.send_message(mail.to_s(), 'DailyBuild@thc.net.cn', to)
+	Net::SMTP.start('smtp.exmail.qq.com', 25, 'smtp.exmail.qq.com', 'jsliu@thc.net.cn', 'Dm2018*') { |smtp|
+    smtp.send_message(mail.to_s(), 'jsliu@thc.net.cn', to)
 }
   puts 'email send out...'
 end
